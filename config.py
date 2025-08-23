65281bab2a5270fa44827ca13094811cc3fa2bcc
Wender1903 <wenderman74@gmail.com>
2025-08-22 21:18:24 -0300

    :sparkles: feat: Configurando banco de dados para se conectar ao banco de dados mysql online que criei no railway

---
+++ config.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['HOST'] = '0.0.0.0'
+app.config['PORT'] = 5001
+app.config['DEBUG'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:ZTGLRCLQDkLxeFadnWNbLUoLNhhbkLfU@caboose.proxy.rlwy.net:17980/railway"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
