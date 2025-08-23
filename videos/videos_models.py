9b17db28fc83d4ba9c9064da6d85be496aa6e7c2
Wender1903 <wenderman74@gmail.com>
2025-08-22 22:26:39 -0300

    :sparkles: feat: Criando rota Post da aplciação e cadastrando um video para testar (o teste do front end, por enquando será feito no meu pc

---
+++ videos/videos_models.py
@@ -0,0 +1,38 @@
+from config import db
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(274), nullable=False)
+    categoria = db.Column(db.String(23), nullable=False)
+    descricao = db.Column(db.String(320), nullable=False)
+    video = db.Column(db.String(1500), nullable=False)
+
+    def __init__(self, nome, categoria, descricao, video):
+        self.nome = nome
+        self.categoria = categoria
+        self.descricao = descricao
+        self.video = video
+
+    def exibir_informacoes(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'categoria': self.categoria,
+            'descricao': self.descricao,
+            'video': self.video
+        }
+
+def criar_videos(novo_video):
+    new_video = Video(
+        nome=str(novo_video['nome']),
+        categoria=str(novo_video['categoria']),
+        descricao=str(novo_video['descricao']),
+        video=str(novo_video['video'])
+    )
+
+    db.session.add(new_video)
+    db.session.commit()
+    return new_video.exibir_informacoes()
+
--- videos/videos_routes.py
+++ videos/videos_routes.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request, jsonify
+from .videos_models import listar_videos, criar_videos
 
 
 videos_blueprint = Blueprint('videos', __name__)
 
-@videos_blueprint.route('/videos', methods=["GET"])
-def listar_videos():
-    return(jsonify("Rota conectada com sucesso rota")), 200
+@videos_blueprint.route('/videos', methods=['POST'])
+def adicionar_video():
+    novo_video = request.json
+
+    new_video = criar_videos(novo_video)
+    return jsonify(new_video), 201
 
